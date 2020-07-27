879b9593a65f63c021a8c221818ebf68ac9e4354
onaryuko <onaryuko@mts.ru>
2020-07-27 16:09:21 +0300

    Initial commit

---
+++ src/main/java/ru/petproject/mobileuser/MobileuserApplication.java
@@ -0,0 +1,13 @@
+package ru.petproject.mobileuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileuserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileuserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/petproject/mobileuser/MobileuserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petproject.mobileuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileuserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
